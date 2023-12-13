469fc4283808ee7c31b0d69609f15a1288938fda
kaungkaung4T <mrkaungminnkhant@gmail.com>
2023-12-13 20:46:00 +0630

    update motion page effect

--- assets/ui/js/page_motion.js
+++ assets/ui/js/page_motion.js
@@ -9,30 +9,24 @@
 
 ScrollReveal().reveal('.motion_item_left', {
     scale: 0.1,
-
     distance: '70px',
     origin: 'left',
-    delay: 100,
     duration: 700,
     reset: true
 });
 
 ScrollReveal().reveal('.motion_item_right', {
     scale: 0.1,
-
     distance: '70px',
     origin: 'right',
-    delay: 100,
     duration: 700,
     reset: true
 });
 
 ScrollReveal().reveal('.motion_item_top', {
     scale: 0.1,
-
     distance: '60px',
     origin: 'top',
-    delay: 300,
     duration: 700,
     reset: true
 });
@@ -41,7 +35,6 @@ ScrollReveal().reveal('.motion_item_bottom', {
     scale: 0.1,
     distance: '60px',
     origin: 'bottom',
-    delay: 300,
     duration: 700,
     reset: true
 });
