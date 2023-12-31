ff025716302c7988e2c70f847ce09e71d45be411
kaungkaung4T <mrkaungminnkhant@gmail.com>
2023-12-31 22:06:27 +0630

    update

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -12,9 +12,13 @@
 
 @foreach ($service as $each_service)
 <a href="{{ route('each_service', $each_service->id) }}" style="margin-right: 7px;margin-bottom:10px">
-    <div class="card" style="width: 12rem;">
-    
-        <img class="card-img-top" src="{{ asset('storage/app/public/'.$each_service->file_path) }}" alt="">
+    <div class="card" style="width: 24rem;">
+
+    <div class="card-img-top" style="align-items:center;text-align:center;">
+        <img class="" 
+        style="object-fit: contain;width:100px;height:100px;" 
+        src="{{ asset('storage/app/public/'.$each_service->file_path) }}" alt="">
+    </div>
     
     <div class="card-body">
         <p class="card-text text-center">{{ $each_service->name }}</p>
