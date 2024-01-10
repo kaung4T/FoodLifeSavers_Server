641b9b62a1e8cb45a6ad04107b338f8c91fe6d7c
kaungkaung4T <mrkaungminnkhant@gmail.com>
2024-01-10 15:46:50 +0630

    fix order admin

--- resources/views/admin/admin_order.blade.php
+++ resources/views/admin/admin_order.blade.php
@@ -19,7 +19,7 @@
 
 
             <div class="mb-4">
-        <h4>Customize Donation Plan</h4>
+        <h4>Donate Subscribers of Donation Plan</h4>
             </div>
 
 
@@ -27,10 +27,10 @@
     <thead>
         <tr>
         <th scope="col">#</th>
-        <th scope="col">Order ID</th>
-        <th scope="col">User ID</th>
-        <th scope="col">User</th>
-        <th scope="col">Payment Type</th>
+        <th scope="col" class="text-center">Order ID</th>
+        <th scope="col" class="text-center">User ID</th>
+        <th scope="col" class="text-center">User</th>
+        <th scope="col" class="text-center">Payment Type</th>
         <th scope="col" class="text-center">Amount</th>
         <th scope="col" class="text-center">Discount</th>
         <th scope="col" class="text-center">Phone</th>
@@ -45,17 +45,22 @@
 
         <tr>
       <th scope="row">{{ $i }}</th>
-                <td>{{ $each_order->order_id }}</td>
-                <td>{{ $each_order->user_id }}</td>
-                <td>{{ $each_order->user }}</td>
+                <td class="text-center">{{ $each_order->order_id }}</td>
+                <td class="text-center">{{ $each_order->user_id }}</td>
+                <td class="text-center">{{ $each_order->user }}</td>
 
-      <td>{{ Str::limit($each_order->payment_plan, 25, $end='...') }}</td>
-      <td class="text-center">{{ $each_order->payment_amount }}</td>
+      <td class="text-center">{{ Str::limit($each_order->payment_plan, 25, $end='...') }}</td>
         
+            @if ($each_order->payment_discount)
+        <td class="text-center" style="color: #565656;">{{ $each_order->payment_amount }}</td>
+            @else
+        <td class="text-center">{{ $each_order->payment_amount }}</td>
+            @endif
+
             @if ($each_order->payment_discount)
         <td class="text-center">{{ $each_order->payment_discount }}</td>
             @else
-        <td class="text-center"> - </td>
+        <td class="text-center" style="color: #565656;"> - </td>
             @endif
 
       <td class="text-center">{{ $each_order->phone }}</td>
