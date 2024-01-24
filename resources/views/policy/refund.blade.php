ba5ed2e748a08f8c5a2a9f9ff822d89ece8e9d0f
kaungkaung4T <mrkaungminnkhant@gmail.com>
2024-01-24 23:52:35 +0630

    update

--- resources/views/policy/refund.blade.php
+++ resources/views/policy/refund.blade.php
@@ -5,7 +5,85 @@
 
 
 <div class="refund_body">
-refund policy
+
+
+<div class="mb-4">
+<h3 style="color: #000;">(Foodlifesavers) REFUND POLICY</h3>
+
+<p class="mt-5">Thank you for purchasing donation plan at (Foodlifesavers)!
+
+We offer refund and/or exchange within the first 30 days of your purchase, if 30 days have passed since your purchase, 
+you will not be offered a refund and/or exchange of any kind.</p>
+</div>
+
+
+
+
+<div class="mb-4">
+<b>Eligibility for Refunds and Exchanges</b>
+
+<ul class="ml-4 mt-1">
+    <li style="list-style: disc;">Your item must be unused and in the same condition that you received it.</li>
+    <li style="list-style: disc;">The item must be in the original packaging.</li>
+    <li style="list-style: disc;">To complete your return, we require a receipt or proof of purchase.</li>
+    <li style="list-style: disc;">Only regular priced items may be refunded, sale items cannot be refunded.</li>
+    <li style="list-style: disc;">If the item in question was marked as a gift when purchased and shipped directly to you, you will receive a gift credit for the value of your return.</li>
+</ul>
+</div>
+
+
+
+<div class="mb-4">
+<b>Exchanges (if applicable)</b>
+
+<p>
+We only replace items if they are defective or damaged. 
+If you need to exchange it for the same item, send us an email at (Relevant Email Address) and 
+send your item to: (Relevant Address).
+</p>
+</div>
+
+
+
+<div class="mb-4">
+<b>Exempt Goods</b>
+
+<p>The following are exempt from refunds:</p>
+<ul class="ml-4 mt-1">
+    <li style="list-style: disc;">Gift cards</li>
+    <li style="list-style: disc;">Some health and personal care items</li>
+</ul>
+</div>
+
+
+
+<div class="mb-4">
+<b>Partial refunds are granted (if applicable)</b>
+
+<ul class="mb-2 ml-4 mt-1">
+    <li style="list-style: disc;">Any item not in its original condition, is damaged or missing parts for reasons not due to our error.</li>
+    <li style="list-style: disc;">Any item that is returned more than 30 days after delivery.</li>
+</ul>
+
+<p>Once your return is received and inspected, we will send you an email to notify you that we have received your returned item. We will also notify you of the approval or rejection of your refund.
+If you are approved, then your refund will be processed, and a credit will automatically be applied to your credit card or original method of payment, within a certain amount of days.
+</p>
+</div>
+
+
+
+<div class="mb-4">
+<b>Late or missing refunds</b>
+
+<ul class="ml-4 mt-1">
+    <li style="list-style: disc;">If you have not received a refund yet, first check your bank account again. Then contact your credit card company, it may take some time before your refund is officially posted.</li>
+    <li style="list-style: disc;">If you have done all of this and you still have not received your refund yet, please contact us at (email and or phone number)</li>
+</ul>
+</div>
+
+
+
+
 </div>
 
 
