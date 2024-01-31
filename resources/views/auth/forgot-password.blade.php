6d63f1a242e11ca4dd7c3db5fe4bf130395add62
kaungkaung4T <mrkaungminnkhant@gmail.com>
2024-02-01 00:56:05 +0630

    update forgot password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,5 +1,34 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
+
+
+
+@extends('main_master2')
+
+@section('content')
+
+    <div class="loginWrapper">
+        <div class="row justify-content-center">
+            <div class="col-md-7 col-lg-5">
+            <div class="card">    
+                <div class="card-header">
+                    <div class="picture">
+                        <img src="{{asset('assets/ui/img/avatar.svg')}}" alt="LoginPicture">
+                    </div>
+                </div>
+
+
+                @if ($errors->has('email'))
+
+                <div class="alert alert-info w-50" role="alert" style="margin: auto;margin-top: 30px;margin-bottom: -30px;">
+            <b class="" role="alert">{{ $errors->first('email') }}</b>
+                </div>
+                
+                @endif
+
+
+        <div class="card-body">
+            
+
+        <div class="mb-4 text-sm text-gray-600">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
 
@@ -10,16 +39,37 @@
         @csrf
 
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+        <div class="form-group">
+            <x-input-label class="col-sm-5" for="email" :value="__('Email')" />
+            <div class="col-sm-7">
+            <x-text-input placeholder="Enter Email" id="email" class="form-control block mt-1" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+            <x-primary-button class="btn btn-primary btn-sm">
+                <!-- {{ __('Email Password Reset Link') }} -->
+                Reset
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+
+
+
+             <div class="mt-3">
+                <div class="flex justify-center items-center">
+                <div class="w-4/6 border border-gray-400 h-0"></div>
+                <div class="w-6/6 mb-[7px] text-center text-white dark:text-white"></div>
+                <div class="w-4/6 border border-gray-400 h-0"></div>
+              </div>
+             </div>
+                </div>
+            </div>
+      
+            </div>
+        </div>
+    </div>
+
+@endsection
+
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,40 @@
-<x-guest-layout>
+
+
+
+@extends('main_master2')
+
+@section('content')
+
+    <div class="loginWrapper">
+        <div class="row justify-content-center">
+            <div class="col-md-7 col-lg-5">
+            <div class="card">    
+                <div class="card-header">
+                    <div class="picture">
+                        <img src="{{asset('assets/ui/img/avatar.svg')}}" alt="LoginPicture">
+                    </div>
+                </div>
+
+
+                @if ($errors->has('email'))
+
+                <div class="alert alert-info w-50" role="alert" style="margin: auto;margin-top: 30px;margin-bottom: -30px;">
+            <b class="" role="alert">{{ $errors->first('email') }}</b>
+                </div>
+                
+                @endif
+
+
+        <div class="card-body">
+            
+
+        <div class="mb-4 text-sm text-gray-600">
+        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    </div>
+
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
     <form method="POST" action="{{ route('password.store') }}">
         @csrf
 
@@ -6,34 +42,58 @@
         <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
+        <div class="form-group">
+            <x-input-label class="col-sm-5" for="email" :value="__('Email')" />
+            <div class="col-sm-7">
+            <x-text-input placeholder="Enter Email" id="email" class="form-control block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+        <div class="form-group mt-4">
+            <x-input-label class="col-sm-5" for="password" :value="__('Password')" />
+            <div class="col-sm-7">
+            <x-text-input placeholder="Enter New Password" id="password" class="form-control block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+        <div class="form-group mt-4">
+            <x-input-label class="col-sm-5" for="password_confirmation" :value="__('Confirm Password')" />
+            <div class="col-sm-7">
+            <x-text-input placeholder="Enter New Password" id="password_confirmation" class="form-control block mt-1 w-full"
                                 type="password"
                                 name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            </div>
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
+            <x-primary-button class="btn btn-primary btn-sm">
+                <!-- {{ __('Reset Password') }} -->
+                Reset
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+
+
+
+             <div class="mt-3">
+                <div class="flex justify-center items-center">
+                <div class="w-4/6 border border-gray-400 h-0"></div>
+                <div class="w-6/6 mb-[7px] text-center text-white dark:text-white"></div>
+                <div class="w-4/6 border border-gray-400 h-0"></div>
+              </div>
+             </div>
+                </div>
+            </div>
+      
+            </div>
+        </div>
+    </div>
+
+@endsection
+
